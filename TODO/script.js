22653e2890bb21d054375eb67dd73e8039948a15
gerproko <101721169+GerProko@users.noreply.github.com>
2022-08-16 23:11:45 -0300

    a

--- TODO/script.js
+++ TODO/script.js
@@ -11,3 +11,19 @@ btnShowLogin.addEventListener("click",()=>{
 btnHideLogin.addEventListener("click",()=>{
     popup.close();
 })
+
+var navbar = document.getElementById("Header") //agarro el header identificandolo por el id y lo guardo en una variable
+
+window.addEventListener("scroll", () =>{ //cuando scrolleo
+    navbar.classList.toggle("sticky", window.scrollY > 0);  //le agrego la clase cuando scrollea
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+    
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    })
+})
